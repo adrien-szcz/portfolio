b2a47f172f3451ca3e7100d4fc1620442b823f29
aszczepaniak <adrien.szczepaniak@etu.u-bordeaux.fr>
2026-05-11 16:37:37 +0900

    setup + begining of the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,81 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+
+import Language from './components/Language';
+
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <BrowserRouter>
 
-      <div className="ticks"></div>
+            {/* Nav Bar */}
+            <nav className='fixed top-0 left-0 w-full z-50 flex items-center justify-between font-bold backdrop-blur-sm'>
+                <p className='text-2xl p-3'>Adrien Szczepaniak</p>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+                <div className='flex gap-3 text-xl *:p-4 *:hover:text-heading'>
+                    <Link to='mailto:adrien10019@gmail.com'>Mail</Link>
+                    <Link to='https://github.com/adrien-szcz'>GitHub</Link>
+                    <Link to='https://www.linkedin.com/in/adrien-szczepaniak/'>Linkedin</Link>
+                </div>                
+            </nav>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+        </BrowserRouter>
+        <div className='pt-10 *:p-5 *:*:my-5'>
+
+          <div> 
+
+            <h1 className='font-bold text-4xl'><span>Welcome to my portfolio !</span></h1>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            
+          </div>
+
+          <div>
+
+            <h1 className='font-bold text-3xl'><span>I.</span> Skills</h1>
+
+            <Language text='Python' imgSource='python.png'></Language>
+
+          </div>
+          
+          <div>
+
+            <h1 className='font-bold text-3xl'><span>II.</span> Projects</h1>
+          
+          </div>
+          
+          <div>
+
+            <h1 className='font-bold text-3xl'><span>III.</span> About me</h1>
+          
+          </div>
+          <div>
+
+            <h1 className='font-bold text-3xl'><span>III.</span> About me</h1>
+          
+          </div>
+          <div>
+
+            <h1 className='font-bold text-3xl'><span>III.</span> About me</h1>
+          
+          </div>
+          <div>
+
+            <h1 className='font-bold text-3xl'><span>III.</span> About me</h1>
+          
+          </div>
+          <div>
+
+            <h1 className='font-bold text-3xl'><span>III.</span> About me</h1>
+          
+          </div>
+
+        </div>
     </>
   )
 }
---
+++ src/utils/type.ts
@@ -0,0 +1,4 @@
+export type LanguageAndPicture = {
+    text: string,
+    imgSource: string
+}
